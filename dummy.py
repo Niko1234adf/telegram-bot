e545e177f4a336f224bd558e87fd744ba3261a5a
Niko1234adf <109465086+Niko1234adf@users.noreply.github.com>
2025-03-23 20:11:04 +0200

    dummy.py

--- dummy.py
+++ dummy.py
@@ -3,5 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running"
+    return "Bot is running."
 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # fallback если PORT не задан
+    app.run(host="0.0.0.0", port=port)
