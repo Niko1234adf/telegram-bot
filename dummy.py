65ce38a54b929a08ceecfedd4e6ca815f4391bf8
Niko1234adf <109465086+Niko1234adf@users.noreply.github.com>
2025-03-25 15:26:56 +0200

    dummy.py

--- dummy.py
+++ dummy.py
@@ -7,5 +7,5 @@ def home():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))  # fallback если PORT не задан
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
