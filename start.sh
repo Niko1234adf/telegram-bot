b0f7440658fe96c2bd17f75774e66c546193ae02
Niko1234adf <109465086+Niko1234adf@users.noreply.github.com>
2025-03-27 21:53:19 +0200

    start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Проверка, не запущен ли бот уже
-if pgrep -f "bot.py" > /dev/null
-then
-    echo "Бот уже запущен"
-else
-    python3 bot.py &
-fi
-
+python3 bot.py &
 gunicorn dummy:app --bind 0.0.0.0:$PORT
