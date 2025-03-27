24ea2912fb184d918d95ab331c6e2ba09f836de2
Niko1234adf <109465086+Niko1234adf@users.noreply.github.com>
2025-03-27 21:27:08 +0200

    start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Сначала запускаем Flask dummy-сервер
-gunicorn dummy:app --bind 0.0.0.0:$PORT &
-
-# Затем запускаем Telegram-бота в основном процессе (без &)
-python3 bot.py
+python3 bot.py &
+gunicorn dummy:app --bind 0.0.0.0:$PORT
